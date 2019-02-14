6755f554cc82c5eafa34d3fff720e305566bad57
Segun <segunolalive@gmail.com>
2019-02-14 13:49:20 +0100

    Updates

---
+++ precache-manifest.fc0897d4160e599020eebc34577054b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a31bfe5675af2786836",
+    "url": "/unsplash-intersect/static/js/runtime~main.8a31bfe5.js"
+  },
+  {
+    "revision": "4e9c039bb16d23ed139a",
+    "url": "/unsplash-intersect/static/js/main.4e9c039b.chunk.js"
+  },
+  {
+    "revision": "3ab973e41c1bad76e8ba",
+    "url": "/unsplash-intersect/static/js/1.3ab973e4.chunk.js"
+  },
+  {
+    "revision": "4e9c039bb16d23ed139a",
+    "url": "/unsplash-intersect/static/css/main.4d81b802.chunk.css"
+  },
+  {
+    "revision": "6b1063d40cb1839015764abf560e7ca2",
+    "url": "/unsplash-intersect/index.html"
+  }
+];
