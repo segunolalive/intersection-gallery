415a148b0678c96d1ffc411d2dff6f9048dd7a46
Segun <segunolalive@gmail.com>
2019-02-18 00:52:03 +0100

    Updates

--- precache-manifest.d4dc5d12739f6d2049a11cdff2a7e2fe.js
+++ precache-manifest.d4dc5d12739f6d2049a11cdff2a7e2fe.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/intersection-gallery/static/js/runtime~main.942844f1.js"
   },
   {
-    "revision": "ce47378f5f497b462bc5",
-    "url": "/intersection-gallery/static/js/main.ce47378f.chunk.js"
+    "revision": "3aaf994fdd5ca65b001e",
+    "url": "/intersection-gallery/static/js/main.3aaf994f.chunk.js"
   },
   {
     "revision": "3ab973e41c1bad76e8ba",
     "url": "/intersection-gallery/static/js/1.3ab973e4.chunk.js"
   },
   {
-    "revision": "ce47378f5f497b462bc5",
+    "revision": "3aaf994fdd5ca65b001e",
     "url": "/intersection-gallery/static/css/main.4d81b802.chunk.css"
   },
   {
-    "revision": "d46a0f934db3b54c74402df1f7e287d5",
+    "revision": "0b52235da2748ec6bcc38f5b03820b05",
     "url": "/intersection-gallery/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/intersection-gallery/precache-manifest.c199a268b34aa13a2ff77f3a8b35c223.js"
+  "/intersection-gallery/precache-manifest.d4dc5d12739f6d2049a11cdff2a7e2fe.js"
 );
 
 workbox.clientsClaim();
